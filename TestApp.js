fc8b81647aec5713e0dca9b8acb710119b4fc5b7
basileu <basil.boli@yahoo.com>
2021-01-12 19:46:46 +0200

    Minor cosmetic log message change to test if push to repo is needed for running heroku locally (heroku local web )
    Signed-off-by: basileu <basil.boli@yahoo.com>

--- TestApp.js
+++ TestApp.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 app.get("", (req, res) => {
   res.send("Hello darkness my old friend");
+  res.send("I've come to talk to you again");
 });
 
 app.listen(PORT, () => {
